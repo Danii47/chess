79c1205c67b0e5b35f086685aa4e2cb671f7b1ad
Danii47 <uranux1@gmail.com>
2024-04-25 01:26:40 +0200

    feat: show possible movements

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,14 +1,31 @@
 import getPieceImage from "../utils/getPieceImage"
+import isValidMove from "../utils/isValidMove"
 
-
-export default function Cell({ rowIndex, cell, cellIndex, onDropHandler, winner }) {
+export default function Cell({ rowIndex, cell, cellIndex, onDropHandler, winner, table, cellSelected, setCellSelected, turn, gameStarted, setGameStarted }) {
 
   const dragStartHandler = (evt, cell) => {
+    if (cell.piece.color !== turn) return
+    if (!gameStarted) setGameStarted(true)
+
     evt.dataTransfer.setData('cell', JSON.stringify(cell))
+    setCellSelected(cell)
   }
 
   const draggingOverHandler = (evt) => {
     evt.preventDefault()
+  }
+
+  const showPossibleMovements = (cellSelected) => {
+    if (cell.piece.color !== turn) return
+    if (!gameStarted) setGameStarted(true)
+
+    setCellSelected(cellSelected)
+  }
+
+  const handleMovePiece = (evt) => {
+    if (!cellSelected) return
+
+    onDropHandler(evt, cellSelected)
   }
 
   return (
@@ -21,13 +38,20 @@ export default function Cell({ rowIndex, cell, cellIndex, onDropHandler, winner 
         (rowIndex === 7) &&
         <div style={{ right: `${cellIndex * 100}px` }} className="chessColLabel">{String.fromCharCode("A".charCodeAt(0) + (7 - cellIndex))}</div>
       }
-      <div id={8 * rowIndex + cellIndex} className={`chessCell ${cell.cellColor}`}>
+      <div
+        id={8 * rowIndex + cellIndex}
+        className={`chessCell ${cell.cellColor} ${cellSelected && isValidMove(table, cellSelected, cell, cellSelected.piece.color) ? "isValidMove" : ""} ${cell.id === cellSelected?.id ? "selected" : ""}`}
+        onClick={(evt) => handleMovePiece(evt)}  
+      >
         {
           cell.piece &&
-            <img
-              className={`chessPiece${(cell.piece && cell.piece.check) ? " checked" : ""}${winner ? " end" : ""}`}
-              src={getPieceImage(cell.piece)} alt={`${cell.piece.type}`}
-              onDragStart={(evt) => dragStartHandler(evt, cell)} />}
+          <img
+            className={`chessPiece${(cell.piece && cell.piece.check) ? " checked" : ""}${winner ? " end" : ""}`}
+            src={getPieceImage(cell.piece)} alt={`${cell.piece.type}`}
+            onDragStart={(evt) => dragStartHandler(evt, cell)}
+            onClick={() => showPossibleMovements(cell)}
+          />
+        }
       </div>
     </div>
   )
