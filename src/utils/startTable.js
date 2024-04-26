df21cd74e3cd3e81baf69761eaa307c6f665b685
Dani47 <87781904+Danii47@users.noreply.github.com>
2024-04-27 01:57:05 +0200

    feat: add getPossibleMoves to piece object

--- src/utils/startTable.js
+++ src/utils/startTable.js
@@ -1,4 +1,5 @@
 import startPieces from './startPieces'
+import isValidMove from './isValidMove'
 
 export default function startTable() {
   const table = []
@@ -14,6 +15,20 @@ export default function startTable() {
         color: i < 2 ? 'black' : 'white',
         check: false,
         hasMoved: false,
+        getPossibleMoves: function (table, cell) {
+
+          const possibleMoves = []
+
+          for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+              if (isValidMove(table, cell, table[i][j], cell.color)) {
+                possibleMoves.push(table[i][j])
+              }
+            }
+          }
+
+          return possibleMoves
+        }
       }
 
       row.push({ id: 8 * i + j, x: j, y: i, cellColor: (i + j) % 2 === 0 ? 'black' : 'white', piece: piece })
