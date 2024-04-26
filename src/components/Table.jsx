b7e6b6ca22eba08e6740bb4ce94610d4eedb93b9
Dani47 <87781904+Danii47@users.noreply.github.com>
2024-04-26 18:03:41 +0200

    feat: add capture and check audio

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -8,29 +8,33 @@ import comprobateCheck from '../utils/comprobateCheck'
 import comprobateCheckMate from '../utils/comprobateCheckMate'
 import pieceMovedSound from '../assets/sounds/pieceMoved.mp3'
 import checkMateSound from '../assets/sounds/checkMate.webm'
+import captureSound from '../assets/sounds/capture.mp3'
+import checkSound from '../assets/sounds/check.mp3'
 
 export default function Table({ table, setTable, turn, setTurn, winner, setWinner, isInCheck, setIsInCheck, gameStarted, setGameStarted }) {
-  
+
   const [cellSelected, setCellSelected] = useState(null)
 
   const pieceMovedAudio = new Audio(pieceMovedSound)
   const checkMateAudio = new Audio(checkMateSound)
+  const captureAudio = new Audio(captureSound)
+  const checkAudio = new Audio(checkSound)
 
   const onDropHandler = (evt, oldCellData = undefined) => {
     if (!oldCellData && !evt.dataTransfer.getData('cell')) return
 
     if (!gameStarted) setGameStarted(true)
-    
+
     const oldCell = oldCellData || JSON.parse(evt.dataTransfer.getData('cell'))
     const tableCopy = JSON.parse(JSON.stringify(table))
-    
+
     const newCellId = evt.target.id || evt.target.parentElement.id
-    
+
     const newCell = tableCopy.flat().find((cell) => {
       return cell.id == newCellId
     })
-    
-    
+
+
     if (!isValidMove(tableCopy, oldCell, newCell, turn)) return
     setCellSelected(null)
 
@@ -38,20 +42,37 @@ export default function Table({ table, setTable, turn, setTurn, winner, setWinne
     const [newX, newY] = [newCell.x, newCell.y]
     const pieceMoved = tableCopy[oldY][oldX].piece
 
+    const newCellPiece = newCell.piece
+
     tableCopy[oldY][oldX].piece = null
     tableCopy[newY][newX].piece = pieceMoved
 
     if ((isInCheck && comprobateCheck(tableCopy, turn === 'white' ? 'black' : 'white', true)) || (!isInCheck && comprobateCheck(tableCopy, turn === 'white' ? 'black' : 'white', true))) return
 
-    
-    setIsInCheck(comprobateCheck(tableCopy, turn, true))
-    
+    const check = comprobateCheck(tableCopy, turn, true)
+
+    setIsInCheck(check)
+
     if (comprobateCheckMate(tableCopy, turn)) {
       setWinner(turn)
       checkMateAudio.play()
-      confetti()
+
+
+      confetti({
+        particleCount: 200,
+        spread: 130,
+        origin: { y: .6 }
+      })
+
+
+
+    } else if (newCellPiece) {
+      captureAudio.play()
+    } else if (check) {
+      checkAudio.play()
     } else {
       pieceMovedAudio.play()
+
     }
 
 
@@ -60,28 +81,28 @@ export default function Table({ table, setTable, turn, setTurn, winner, setWinne
   }
 
   return (
-    
+
     <div className="chessTable shadow">
       {
         winner &&
-          <Winner winner={winner}/>
+        <Winner winner={winner} />
       }
-      
+
       {
         table.map((row, rowIndex) => {
 
           return <Row
-              key={rowIndex}
-              row={row}
-              rowIndex={rowIndex}
-              onDropHandler={onDropHandler}
-              table={table}
-              cellSelected={cellSelected}
-              setCellSelected={setCellSelected}
-              turn={turn}
-              gameStarted={gameStarted}
-              setGameStarted={setGameStarted}
-            />
+            key={rowIndex}
+            row={row}
+            rowIndex={rowIndex}
+            onDropHandler={onDropHandler}
+            table={table}
+            cellSelected={cellSelected}
+            setCellSelected={setCellSelected}
+            turn={turn}
+            gameStarted={gameStarted}
+            setGameStarted={setGameStarted}
+          />
 
         })
       }
