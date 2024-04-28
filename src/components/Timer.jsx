965fd4d79997b75385e7a7f80e5d6a2fea5b5cf4
Dani47 <87781904+Danii47@users.noreply.github.com>
2024-04-28 16:03:26 +0200

    feat: activate ia button

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 
-export default function Timer({ winner, setWinner, turn, time, setTime, gameStarted }) {
+export default function Timer({ winner, setWinner, turn, time, setTime, gameStarted, IAOpponent, setIAOpponent }) {
 
 
 
@@ -12,8 +12,8 @@ export default function Timer({ winner, setWinner, turn, time, setTime, gameStar
 
       const newSeconds = time[turn].seconds === 0 ? 59 : time[turn].seconds - 1
       const newMinutes = time[turn].seconds === 0 ? time[turn].minutes - 1 : time[turn].minutes
-      
-      setTime({ 
+
+      setTime({
         ...time,
         [turn]: {
           seconds: newSeconds,
@@ -22,18 +22,25 @@ export default function Timer({ winner, setWinner, turn, time, setTime, gameStar
       })
 
       if (newSeconds === 0 && newMinutes === 0) setWinner(turn === 'white' ? 'black' : 'white')
-      
+
 
     }, 1000);
 
     return () => clearInterval(interval);
 
   }, [winner, setWinner, turn, time, setTime, gameStarted])
-  
+
+  const handleActivateIA = () => {
+    setIAOpponent(!IAOpponent)
+  }
+
   return (
     <div className="timer">
-      <div className={`player shadow${turn === "black" ? " active" : ""}`}>
-        <div className="playerTime">{`${time.black.minutes >= 10 ? time.black.minutes : `0${time.black.minutes}`}`}:{`${time.black.seconds >= 10 ? time.black.seconds : `0${time.black.seconds}`}`}</div>
+      <div>
+        <div className={`player shadow${turn === "black" ? " active" : ""}`}>
+          <div className="playerTime">{`${time.black.minutes >= 10 ? time.black.minutes : `0${time.black.minutes}`}`}:{`${time.black.seconds >= 10 ? time.black.seconds : `0${time.black.seconds}`}`}</div>
+        </div>
+        <button type="checkbox" className={`activateIA ${IAOpponent ? "activated" : ""}`} onClick={handleActivateIA}>{IAOpponent ? "Desactivar" : "Activar"} IA</button>
       </div>
       <div className={`player shadow${turn === "white" ? " active" : ""}`}>
         <div className="playerTime">{`${time.white.minutes >= 10 ? time.white.minutes : `0${time.white.minutes}`}`}:{`${time.white.seconds >= 10 ? time.white.seconds : `0${time.white.seconds}`}`}</div>
